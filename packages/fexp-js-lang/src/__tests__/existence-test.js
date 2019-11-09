88a5fb472fa2e1f6d9b5184bc17f0542566024a0
Cam Manderson <cam@alpaca.travel>
2019-11-09 17:05:46 +1100

    Fixes broken test

--- packages/fexp-js-lang/src/__tests__/existence-test.js
+++ packages/fexp-js-lang/src/__tests__/existence-test.js
@@ -2,16 +2,16 @@ const existence = require("../existence");
 
 describe("Existence", () => {
   it('will check properties using "has"', () => {
-    expect(existence.has("foo")).toBe(true);
-    expect(existence.has(undefined)).toBe(false);
-    expect(existence.has(null)).toBe(true);
+    expect(existence.has(["foo"])).toBe(true);
+    expect(existence.has([undefined])).toBe(false);
+    expect(existence.has([null])).toBe(true);
   });
   it('will check elements exist using "exists"', () => {
-    expect(existence.exists("foo")).toBe(true);
-    expect(existence.exists(undefined)).toBe(false);
-    expect(existence.exists(null)).toBe(false);
+    expect(existence.exists(["foo"])).toBe(true);
+    expect(existence.exists([undefined])).toBe(false);
+    expect(existence.exists([null])).toBe(false);
   });
   it('will check empty values using "empty"', () => {
-    expect(existence.empty(null)).toBe(true);
+    expect(existence.empty([null])).toBe(true);
   });
 });
