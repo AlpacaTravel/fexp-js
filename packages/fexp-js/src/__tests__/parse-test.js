e3506a733bae05b9651536f27b775346d9696f0a
Cam Manderson <cam@alpaca.travel>
2019-11-16 22:35:39 +1100

    Adds in a refactor to use a parser to construct the expression, with the aim of checking types and compatible return types through simple functions

---
+++ packages/fexp-js/src/__tests__/parse-test.js
@@ -0,0 +1,174 @@
+const parse = require("../parse");
+
+describe("Parse", () => {
+  describe("getFunctionName(expr)", () => {
+    it("will extract a function name", () => {
+      expect(parse.getFunctionName(["fn"])).toBe("fn");
+      expect(parse.getFunctionName(["!fn"])).toBe("fn");
+    });
+  });
+  describe("isExpression(expr)", () => {
+    it("will validate expressions", () => {
+      expect(parse.isExpression(["fn"])).toBe(true);
+      expect(parse.isExpression(["fn", 1, 2])).toBe(true);
+
+      expect(parse.isExpression([() => {}])).toBe(false);
+      expect(parse.isExpression([])).toBe(false);
+      expect(parse.isExpression(false)).toBe(false);
+      expect(parse.isExpression(() => {})).toBe(false);
+    });
+  });
+  describe("getFunction(name,lang)", () => {
+    it("will return the method", () => {
+      const fn = () => {};
+      expect(parse.getFunction("fn", { fn })).toBe(fn);
+      expect(parse.getFunction("fn", {})).toBeUndefined();
+      expect(() => parse.getFunction("fn")).toThrow();
+    });
+  });
+  describe("getParser(lang, opts)", () => {
+    it("will create a parser", () => {
+      const fn = () => true;
+      const lang = { fn };
+      const options = {};
+      const parser = parse.getParser(lang, options);
+      expect(typeof parser).toBe("function");
+      expect(parser.lang).toBe(lang);
+      expect(parser.options).toBe(options);
+    });
+  });
+  describe("getArgumentsParser()", () => {
+    it("will return a default parser that calls the parser for each argument and validates the types", () => {
+      const parser = jest.fn();
+      const fn = () => {};
+
+      const argumentParser = parse.getArgumentsParser(fn, parser);
+      expect(typeof argumentParser).toBe("function");
+
+      // Invoke
+      const result = argumentParser([1, 2]);
+
+      expect(parser.mock.calls.length).toBe(2);
+      expect(parser.mock.calls[0][0]).toBe(1);
+      expect(parser.mock.calls[1][0]).toBe(2);
+      expect(result.length).toBe(2);
+    });
+
+    it("will defer the parser to control of the function if it implements", () => {
+      const parser = jest.fn();
+      const fn = () => {};
+      fn.parse = (args, p) => {
+        const [arg0, arg1] = args;
+        return [p(arg0 * 2), p(arg1 * 2)];
+      };
+
+      const argumentParser = parse.getArgumentsParser(fn, parser);
+      expect(typeof argumentParser).toBe("function");
+
+      // Invoke
+      const result = argumentParser([1, 2]);
+
+      expect(parser.mock.calls.length).toBe(2);
+      expect(parser.mock.calls[0][0]).toBe(2);
+      expect(parser.mock.calls[1][0]).toBe(4);
+      expect(result.length).toBe(2);
+    });
+  });
+  describe("composeRuntimeContextArguments(context, parsedArguments)", () => {
+    it("will supply a generator that invokes the arguments", () => {
+      // const arg0 = jest.fn(i => i.foo);
+      const arg0 = jest.fn(i => i.foo);
+      const arg1 = jest.fn(() => 2);
+
+      const context = { foo: 1 };
+
+      const runtime = parse.composeRuntimeContextArguments(context, [
+        arg0,
+        arg1
+      ]);
+
+      expect(typeof runtime.get).toBe("function");
+
+      expect(runtime.get(0)).toBe(1);
+      expect(runtime.get(1)).toBe(2);
+
+      const [a0, a1] = runtime;
+      expect(a0).toBe(1);
+      expect(a1).toBe(2);
+
+      expect(runtime.context).toBe(context);
+      expect(runtime.arguments()).toMatchObject([1, 2]);
+
+      const runtime2 = runtime.createSubRuntime({ foo: 10 });
+      expect(runtime2.get(0)).toBe(10);
+    });
+  });
+  describe("getInvocableFunction(fn, parsedArguments, negate)", () => {
+    it("will execute correctly", () => {
+      const fn = ([arg0, arg1]) => arg0 === arg1;
+      const args = [
+        jest.fn(context => context.foo),
+        jest.fn(context => context.bar)
+      ];
+
+      expect(
+        parse.getInvocableFunction(fn, args, false)({ foo: true, bar: true })
+      ).toBe(true);
+      expect(
+        parse.getInvocableFunction(fn, args, true)({ foo: true, bar: true })
+      ).toBe(false);
+      expect(
+        parse.getInvocableFunction(fn, args, false)({ foo: false, bar: true })
+      ).toBe(false);
+      expect(
+        parse.getInvocableFunction(fn, args, false)({ foo: false, bar: false })
+      ).toBe(true);
+      expect(
+        parse.getInvocableFunction(fn, args, true)({ foo: false, bar: false })
+      ).toBe(false);
+    });
+  });
+  describe("parse", () => {
+    it("will execute functions correctly", () => {
+      const fnA = ([a, b]) => a === b;
+      const fnB = () => true;
+      const fnC = () => true;
+      const lang = { "fn-a": fnA, "fn-b": fnB, "fn-c": fnC };
+
+      const expr = ["fn-a", ["fn-b"], ["fn-c"]];
+      expect(parse.parse(expr, lang)()).toBe(true);
+    });
+
+    it("will execute functions with arguments", () => {
+      const fnA = ([a, b]) => a === b;
+      const fnB = args => args.context[args.get(0)];
+      fnB.parse = args => [args[0]];
+      const fnC = () => "bar";
+      const lang = { "fn-a": fnA, "fn-b": fnB, "fn-c": fnC };
+
+      const expr = ["fn-a", ["fn-b", "foo"], ["fn-c"]];
+      expect(parse.parse(expr, lang)({ foo: "bar" })).toBe(true);
+
+      const expr1 = ["!fn-a", ["fn-b", "foo"], ["fn-c"]];
+      expect(parse.parse(expr1, lang)({ foo: "bar" })).toBe(false);
+    });
+
+    it("will delay execution", () => {
+      const fnA = args => {
+        for (arg of args) {
+          if (arg === true) {
+            return true;
+          }
+        }
+      };
+      const fnB = () => true;
+      const fnC = () => {
+        throw Error("Should not be thrown");
+      };
+      const lang = { "fn-a": fnA, "fn-b": fnB, "fn-c": fnC };
+
+      const expr = ["fn-a", ["fn-b"], ["fn-c"]];
+      expect(parse.parse(expr, lang)()).toBe(true);
+    });
+  });
+});
---
+++ packages/fexp-js/src/parse.js
@@ -0,0 +1,272 @@
+const assert = require("assert");
+
+// Create the structure, without executing the params
+
+// fn.parse(args, parser) // returns args invokeables (Literal can stop descending)
+// fn.argumentTypes[] // Validates each of the args return types
+// fn.returns[] // Used for validation
+
+// Look at the expression
+// Map to the function
+// IF literal, bypass recurse (var,const,let assignments?)
+// IF FN encountered
+// Wrap a future context to reset the vars argument
+// IF undefined return? Omit?, process without a return argument (e.g. filter out)
+// Recurse parse the arguments (DFS to end) to obtain runtimes
+// Validate as you go (first pass)
+// Return the invokeable version, that takes a future context
+
+// Rerun the invokeable version with the context
+
+// const myMethod = ([a, b]) => a + b;
+// myMethod.argumentTypes = [PropTypes.number.isRequired, PropTypes.number.isRequired]
+
+// const literal = ([a]) => a;
+// literal.parse = (args) => {
+//   const parsed = 0
+// }
+
+// const literal = (context) => context.arg(0);
+// literal.create = (args) => {
+//   return [args[0]];
+// }
+// literal.returns = [types.any];
+// literal.argumentTypes = [types.any];
+
+// const sum = (context) => context.args().reduce((c, t) => c + t, 0);
+// sum.returns = [types.number];
+// sum.argumentTypes = [types.number];
+
+// create: (args, parser) => {
+//   const [arg0, arg1] = args;
+
+//   return [() => {}, [parser.parse(arg0, 0), parser.parse(arg1, 1)]]
+// },
+
+// argumentTypes: [
+//   types.anyOf(types.number),
+//   types.number,
+// ],
+// returns: [types.number, types.bool]
+
+// Parse an expression
+const parse = (expr, lang, options) => {
+  try {
+    // Ensure we have valid expression syntax
+    assertValidExpressionSyntax(expr);
+
+    // Identify parts
+    const name = getFunctionName(expr);
+    const args = getFunctionArguments(expr);
+    const negate = isFunctionNegated(expr);
+
+    // Obtain the fn
+    const fn = getFunction(name, lang);
+    assertValidExpressionFunction(fn);
+
+    // Obtain the fn parser for arguments
+    const parser = getArgumentsParser(fn, getParser(lang, options));
+    const parsedArguments = parser(args);
+
+    // Evaulate the response
+    return isFunction(parsedArguments)
+      ? getInvocableFunction(parsedArguments, [], negate)
+      : getInvocableFunction(fn, parsedArguments, negate);
+  } catch (e) {
+    console.error(`Parse error: ${e.message}`);
+    throw e;
+  }
+};
+
+const getParser = (lang, options) => {
+  const parser = expr => parse(expr, lang, options);
+
+  parser.lang = lang;
+  parser.options = options;
+
+  return parser;
+};
+
+// Takes a function and the parsed arguments
+const getInvocableFunction = (fn, parsedArguments, negate) => {
+  assertValidRuntimeFunction(fn);
+
+  // Build in context
+  const invocableFunction = function(context) {
+    // Args are supplied, or embedded
+    const functionArguments = composeRuntimeContextArguments(
+      context,
+      parsedArguments
+    );
+
+    // Supply the runnable context
+    const result = fn(functionArguments);
+
+    // Optionally negate the result
+    if (negate) {
+      return !result;
+    }
+
+    return result;
+  };
+
+  // Parse reference to the runnable return type
+  invocableFunction.returns = fn.returns;
+
+  return invocableFunction;
+};
+
+// Compose a context surrounding the supplied arguments
+const composeRuntimeContextArguments = (context, parsedArguments) => {
+  // Factory a runtime..
+  const factoryRuntime = (context, contextParent) => {
+    const runtime = {};
+
+    runtime.get = get(runtime);
+    const iterator = getIterator(runtime);
+    runtime[Symbol.iterator] = iterator;
+    runtime.contextParent = contextParent;
+    runtime.context = context;
+    runtime.arguments = () => [...iterator()];
+    runtime.length = parsedArguments.length;
+
+    // Provide an ability to create sub runtime for the args
+    runtime.createSubRuntime = ctx => factoryRuntime(ctx, context);
+
+    return runtime;
+  };
+
+  // Generator to invoke obtaining the argument result
+  const get = runtime =>
+    function(index) {
+      const argument = parsedArguments[index];
+      if (typeof argument === "function") {
+        return argument(runtime.context); // Add runtime context
+      } else {
+        return argument;
+      }
+    };
+
+  // An iterator to process over arguments
+  const getIterator = runtime =>
+    function*() {
+      let x = 0;
+      while (x < parsedArguments.length) {
+        yield runtime.get(x++);
+      }
+    };
+
+  // Build the runtime
+  return factoryRuntime(context);
+};
+
+// Obtain the parser for a function
+const getArgumentsParser = (obj, parser) => {
+  // Setup a curryable instance
+  const parserLocal = expr => parser(expr);
+
+  // Provide a validate types aware of the object
+  parserLocal.validateTypes = args => {
+    // TODO: Validate the types
+    // Take the resolved args, and see if they have corresponding return types
+  };
+
+  parserLocal.lang = parser.lang;
+  parserLocal.options = parser.options;
+
+  // Evaluate the parsing function
+  return (() => {
+    // Object provides a parsing implementation
+    if (isFunction(obj.parse)) {
+      return args => obj.parse(args, parserLocal);
+    }
+
+    // Parse using the default parsing behaviour
+    return args => {
+      const parsedArguments = args.map(parser);
+
+      parserLocal.validateTypes(parsedArguments);
+
+      return parsedArguments;
+    };
+  })();
+};
+
+// Evaluate that the supplied expression is in the correct syntax structure
+const isExpression = expr =>
+  Array.isArray(expr) && expr.length > 0 && typeof expr[0] === "string";
+
+// Determins if supplied arg is a function
+const isFunction = fn => typeof fn === "function";
+
+const isFunctionNegated = ([name]) => name.substr(0, 1) == "!";
+
+// Obtain the function name from the expression
+const getFunctionName = expr => {
+  const [name] = expr;
+
+  // Allow for negate syntax
+  if (name.substr(0, 1) === "!") {
+    return name.substr(1);
+  }
+
+  return name;
+};
+
+// Returns the function arguments
+const getFunctionArguments = ([, ...args]) => args;
+
+// Obtain the function from the language
+const getFunction = (name, lang) => {
+  assertValidLanguage(lang);
+
+  if (lang[name]) {
+    return lang[name];
+  }
+
+  switch (name) {
+    case "!":
+      return negate;
+    default:
+      break;
+  }
+
+  return undefined;
+};
+
+// Assert a check on the expression syntax
+const assertValidExpressionSyntax = expr =>
+  assert(
+    isExpression(expr),
+    "Expression should be an array with the first argument being a string"
+  );
+
+// Assert a check on the language
+const assertValidLanguage = lang =>
+  assert(typeof lang === "object", "Language mapping is not present");
+
+// Assert a check on the function
+const assertValidExpressionFunction = fn =>
+  assert(typeof fn === "function", "Language function is not valid");
+
+const assertValidRuntimeFunction = obj => {
+  assert(obj, "Runtime not found");
+  if (typeof obj === "function") {
+    return;
+  }
+  if (typeof obj.run === "function") {
+    return;
+  }
+  throw new Error("Invalid run function");
+};
+
+module.exports = {
+  getFunctionName,
+  isExpression,
+  getFunction,
+  getArgumentsParser,
+  getParser,
+  composeRuntimeContextArguments,
+  getInvocableFunction,
+  parse
+};
